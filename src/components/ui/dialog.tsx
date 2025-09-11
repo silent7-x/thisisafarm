39635bab9f8014d6f9e7a268ba199b5cb95fc46e
silen7-pool <mo7trading@gmail.com>
2025-09-11 22:51:32 +0100

    Refine close button styling in Dialog component for better alignment and responsiveness

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -67,7 +67,7 @@ function DialogContent({
         {showCloseButton && (
           <DialogPrimitive.Close
             data-slot="dialog-close"
-            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-2 right-1 sm:top-1 sm:right-2  opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-6 border border-muted-foreground rounded-full"
+            className="data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 sm:top-3 right-4  opacity-70 transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 sm:[&_svg:not([class*='size-'])]:size-5 border border-muted-foreground rounded-full p-0.5"
           >
             <XIcon />
             <span className="sr-only">Close</span>
